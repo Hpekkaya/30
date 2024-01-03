2fc4c51c0f5f056994d399fcc19f75c29d88fad7
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-03 19:13:19 +0300

    spread operator

--- trial.js
+++ trial.js
@@ -37,6 +37,49 @@ let text=""
 // const {name: name1, age, like} = person
 // console.log(name1,age, like)
 
+// const vehicleOne = {
+//     brand: "Ford",
+//     model: "Mustang",
+//     type: "car",
+//     year: 2023,
+//     color:"red"
+// }
+// function myVehicle(vehicle){
+//     const message = "My" + vehicle.type + " is a " + vehicle.color + " " + vehicle.brand + " " + vehicle.model + "."
+//     console.log(message)
+// }
+// myVehicle(vehicleOne)
 
+// function myVehicle({type,color,brand,model}){
+//     const message = "My " + type + " is a " + color + " " + brand + " " + model + "."
+//     console.log(message)
+
+// }
+// myVehicle(vehicleOne)
+
+
+// spread operator
+
+// const numberOne = [1,2,3]
+// const numberTwo = [4,5,6]
+// const numbersCombined = [...numberOne,...numberTwo,7,8]
+// console.log(numbersCombined)
+
+// const numbers = [1,2,3,4,5,6]
+// const [a,b,...rest] = numbers
+// console.log(rest)
+
+const myVehicle = {
+    brand: "ford",
+    model: "mustang",
+    color: "red"
+}
+const updateVehicle = {
+    type: "car",
+    year: 2023,
+    color: "yellow"
+}
+const updated = {...myVehicle,...updateVehicle}
+console.log(updated)
 
 
