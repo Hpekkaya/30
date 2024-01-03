8216ee26ab761e8d8dff618313773ec896a0d93c
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-03 17:40:10 +0300

    For loops tried

---
+++ trial.js
@@ -0,0 +1,33 @@
+//Destructing
+
+const vehicles = ["Ford","Mustang","Fiat"]
+// const vehicle1 = vehicles[0]
+// const vehicle2 = vehicles[1]
+// const vehicle3 = vehicles[2]
+// console.log(vehicle1)
+
+const [,vehicle1,] = vehicles
+// console.log(vehicle1)
+
+let text=""
+
+// vehicles.forEach(myFunction)
+// function myFunction(i, index) 
+// {text += index + ": " +i+ "\n";} 
+
+
+// for (let i = 0; i < vehicles.length; i++)
+// {text += i + ": " +vehicles[i]+ "\n";} 
+
+// for (let i in vehicles) {
+//     text += i + " : " + vehicles[i] +"\n"
+// }
+
+
+// for (let x of vehicles)
+// {text += x + "\n";} 
+
+
+
+
+console.log(text)
