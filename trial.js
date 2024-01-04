92ec82a16be51fea4e86dd898df614c9d6023cad
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-04 09:56:25 +0300

    constructor tried

--- trial.js
+++ trial.js
@@ -1,85 +1,14 @@
-//Destructing
+// destructing 
 
-const vehicles = ["Ford","Mustang","Fiat"]
-// const vehicle1 = vehicles[0]
-// const vehicle2 = vehicles[1]
-// const vehicle3 = vehicles[2]
-// console.log(vehicle1)
+// const vehicles = ["Ford", "Nissan", "Toyota"]
+// const [a,b,] = vehicles
+// console.log(b)
 
-const [,vehicle1,] = vehicles
-// console.log(vehicle1)
-
-let text=""
-
-// vehicles.forEach(myFunction)
-// function myFunction(i, index) 
-// {text += index + ": " +i+ "\n";} 
-
-
-// for (let i = 0; i < vehicles.length; i++)
-// {text += i + ": " +vehicles[i]+ "\n";} 
-
-// for (let i in vehicles) {
-//     text += i + " : " + vehicles[i] +"\n"
+// const person= {
+//     name :"Hakan",
+//     age  : 25,
+//     location : "Turkey",
+//     like : "Travel"
 // }
-
-
-// for (let x of vehicles)
-// {text += x + "\n";} 
-// console.log(text)
-
-// const person = {
-//     name: "Mustafa",
-//     age: 25,
-//     location: "Ankara",
-//     like: "Pizza"
-// }
-// const {name: name1, age, like} = person
-// console.log(name1,age, like)
-
-// const vehicleOne = {
-//     brand: "Ford",
-//     model: "Mustang",
-//     type: "car",
-//     year: 2023,
-//     color:"red"
-// }
-// function myVehicle(vehicle){
-//     const message = "My" + vehicle.type + " is a " + vehicle.color + " " + vehicle.brand + " " + vehicle.model + "."
-//     console.log(message)
-// }
-// myVehicle(vehicleOne)
-
-// function myVehicle({type,color,brand,model}){
-//     const message = "My " + type + " is a " + color + " " + brand + " " + model + "."
-//     console.log(message)
-
-// }
-// myVehicle(vehicleOne)
-
-
-// spread operator
-
-// const numberOne = [1,2,3]
-// const numberTwo = [4,5,6]
-// const numbersCombined = [...numberOne,...numberTwo,7,8]
-// console.log(numbersCombined)
-
-// const numbers = [1,2,3,4,5,6]
-// const [a,b,...rest] = numbers
-// console.log(rest)
-
-const myVehicle = {
-    brand: "ford",
-    model: "mustang",
-    color: "red"
-}
-const updateVehicle = {
-    type: "car",
-    year: 2023,
-    color: "yellow"
-}
-const updated = {...myVehicle,...updateVehicle}
-console.log(updated)
-
-
+// const {name,age:age1,like}= person
+// console.log(age1)
